dd8b7f21d9025995dabd3133d36887473554fd93
surfmoss <surfmoss@gmail.com>
2018-11-10 10:35:38 -0800

    rm

--- TCL/foreach-rate.tcl
+++
@@ -1,1 +0,0 @@
-foreach var {40 40 40 40 40 40 40 40 40 40 40 40 40} {exec show interface e1/$var | i rate}
--- TCL/proc-rate.tcl
+++
@@ -1,6 +0,0 @@
-proc int_rate {x} {
- for {set n 1} {$n<=$x} {incr n 0} {
-    exec "show interface e1/$n | i rate"
-    exec "show clock"
- }
-}
