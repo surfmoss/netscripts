64d74199865f9240480a51368af4ef546c72285a
surfmoss <surfmoss@gmail.com>
2018-11-10 09:49:59 -0800

    mature rate script

--- rate.tcl
+++ rate.tcl
@@ -1,1 +1,6 @@
-foreach var {40 40 40 40 40 40 40 40 40 40 40 40 40} {exec show interface e1/$var | i rate}
+proc int_rate {x} {
+ for {set n 1} {$n<=$x} {incr n 0} {
+    exec "show interface e1/$n | i rate"
+    exec "show clock"
+ }
+}
