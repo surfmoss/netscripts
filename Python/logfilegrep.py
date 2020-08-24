eda8da39c33d52ae410f465fd05ad06516bf5723
surfmoss <31913954+surfmoss@users.noreply.github.com>
2020-08-24 13:41:39 -0400

    Update logfilegrep.py

--- Python/logfilegrep.py
+++ Python/logfilegrep.py
@@ -23,7 +23,7 @@ Enter the source or destination IP that is in question: 192.168.1.1
 </pre>
 
 <pre>
-id=firewall time="2016-01-14 03:10:09" fw=sledge2.fnmoc.navy.mil pri=6 rule="loopback zone
+id=firewall time="2016-01-14 03:10:09" fw=firewall.mil pri=6 rule="loopback zone
  2 to DNS severs" proto=kernel sent=134 rcvd=40 src=127.2.0.1 srcname= dst=192.168.1.1 dst
 name= user= msg="Network Traffic Event"
 id=firewall time="2016-01-14 05:11:15" fw=firewall.mil pri=3 proto=110/tcp 
